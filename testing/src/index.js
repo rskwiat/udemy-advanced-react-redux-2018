c725625342feb9036b47cc78f90526c28fdb2cc7
Robert Skwiat <robert.skwiat@mastercard.com>
2019-02-11 13:55:22 -0500

    initial source files

---
+++ testing/src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Application Starter</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
