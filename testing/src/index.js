790b5cda991d955117dfc6a9fb6d20e98a300352
Robert Skwiat <robert.skwiat@mastercard.com>
2019-02-28 11:14:34 -0500

    updating project files -- including proper cra

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => (
-  <div>
-    <h1>Application Starter</h1>
-  </div>
-);
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
