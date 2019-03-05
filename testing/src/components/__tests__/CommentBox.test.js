9ac4d9b36de4dddc89679d409f7524a13acf7463
Robert Skwiat <robert.skwiat@mastercard.com>
2019-03-05 14:51:24 -0500

    finishing up the dev test

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -19,9 +19,9 @@ describe('The Comment Box Component', () => {
     component.unmount();
   });
 
-  it('shows a textarea and button', () => {
+  it('shows a textarea and two buttons', () => {
     expect(component.find('textarea').length).toEqual(1);
-    expect(component.find('button').length).toEqual(1);
+    expect(component.find('button').length).toEqual(2);
   });
 
   describe('the text area', () => {
