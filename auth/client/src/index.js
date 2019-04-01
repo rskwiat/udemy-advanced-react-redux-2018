e27500813bf10de55c6d51b5930b076d09a6ed6b
Robert Skwiat <robert.skwiat@mastercard.com>
2019-04-01 11:22:53 -0400

    adding mongodb shared mongodb to project

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore , applyMiddleware } from 'redux';
-import reduxThunk from 'redx-thunk';
+import thunk from 'redx-thunk';
 
 import App from "./components/app";
 import Welcome from './components/welcome';
 import Signup from './components/auth/signup';
 import reducers from './reducers';
 
-const store = createStore(reducers, {});
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
